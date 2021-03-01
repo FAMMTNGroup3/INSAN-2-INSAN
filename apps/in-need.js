05ff282046dfa2f89a67c7eb4a70cbe77cfeef36
ahmad shalan <engashalan@gmail.com>
2021-03-01 10:38:52 +0200

    project2

--- apps/in-need.js
+++ apps/in-need.js
@@ -20,9 +20,16 @@ function handleUserSubmit(event) {
     userAgree = document.getElementById("agree").checked;
     // console.log(userAgree);
    new InNeedUsers(typeOfNeed, phoneNo, description, userAgree);
-}
 
-// userSubmit.setAttribute("onclick", "this.disabled = true");
+   createOrganization();
+}
+userSubmit.setAttribute("onclick","this.disabled = true");
+
+
+let gettingItem = localStorage.getItem("newOrganization");
+
+let normal = JSON.parse(gettingItem);
+console.log(normal);
 
 function InNeedUsers(typeOfNeed, phoneNo, description, userAgree) {
     this.typeOfNeed = typeOfNeed;
@@ -31,4 +38,42 @@ function InNeedUsers(typeOfNeed, phoneNo, description, userAgree) {
     this.userAgree = userAgree;
     userInNeed.push(this);
     console.log(userInNeed);
+}
+
+let parent = document.getElementById('organizationList');
+
+function createOrganization (){
+for (let i=0 ; i< normal.length ; i++){
+
+
+    if (typeOfNeed === normal[i].services){
+    
+    let createList = document.createElement('li');
+
+        
+        let createImg = document.createElement('img');
+        let createSrc = document.createAttribute('src')
+        createImg.setAttributeNodeNS(createSrc)
+        
+        let createH2ForName = document.createElement('h2');
+        let createParaForDescription = document.createElement('p');
+        let serviceType = document.createElement('p')
+        let createParaForInfo = document.createElement('p');
+
+        createH2ForName.textContent = normal[i].name
+        createParaForDescription.textContent = normal[i].description
+        serviceType.textContent = normal[i].services
+        createParaForInfo.textContent = normal[i].info
+        createImg.src = normal[i].imagePath
+
+        createList.appendChild(createImg);
+        createList.appendChild(createH2ForName);
+        createList.appendChild(createParaForDescription);
+        createList.appendChild(serviceType);
+        createList.appendChild(createParaForInfo);
+        parent.appendChild(createList);
+        
+    }
+
+}
 }
